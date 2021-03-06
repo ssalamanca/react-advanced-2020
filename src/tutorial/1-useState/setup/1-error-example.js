add7c860dd539d44a219a9667c9905680444b794
Santiago Salamanca <santiagosw18@gmail.com>
2021-03-06 11:14:25 -0500

    setup - 1-errorExampe changed

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let randomTitle = 'ramdom thing';
+  function handleClick(){
+    alert("button pressed");
+  }
+  return (
+    <React.Fragment>
+      <h2>{randomTitle}</h2>
+      <button className='btn' onClick={handleClick}>Press the button</button>
+    </React.Fragment>
+  );
 };
 
 export default ErrorExample;
